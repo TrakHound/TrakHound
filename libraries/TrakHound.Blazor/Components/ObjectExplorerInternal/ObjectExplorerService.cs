6c9e34001bd69356f0372e50a95cb8c4d8ca1fae
Patrick Ritchie <Patrick@TrakHound.com>
2024-11-14 14:13:22 -0500

    Updated TreeView

--- libraries/TrakHound.Blazor/Components/ObjectExplorerInternal/ObjectExplorerService.cs
+++ libraries/TrakHound.Blazor/Components/ObjectExplorerInternal/ObjectExplorerService.cs
@@ -150,8 +150,8 @@ namespace TrakHound.Blazor.Components.ObjectExplorerInternal
             _recentTimer.Elapsed += ProcessRecentValues;
             _recentTimer.Start();
 
-            _contentUpdateThrottle = new DelayEvent(500);
-            _contentUpdateThrottle.Elapsed += ContentUpdateThrottleElapsed;
+            //_contentUpdateThrottle = new DelayEvent(500);
+            //_contentUpdateThrottle.Elapsed += ContentUpdateThrottleElapsed;
 
             _selectedObjectLoadDelay = new DelayEvent(500);
             _selectedObjectLoadDelay.Elapsed += SelectedObjectLoadDelayElapsed;
@@ -401,6 +401,26 @@ namespace TrakHound.Blazor.Components.ObjectExplorerInternal
         }
 
 
+        public void ClearTree()
+        {
+            _uuid = null;
+            _previousUuid = null;
+            _query = null;
+            _previousQuery = null;
+            _selectedObject = null;
+            _targetUuids.Clear();
+            _expandedNamespaces.Clear();
+            _expandedObjects.Clear();
+            _objects.Clear();
+            _objectParents.Clear();
+            _objectChildCount.Clear();
+            _definitions.Clear();
+            _definitionParents.Clear();
+            _definitionDescriptions.Clear();
+            _sourceChains.Clear();
+        }
+
+
         public async Task LoadByExpression(string query, bool forceReload = false)
         {
             if (string.IsNullOrEmpty(query)) _query = "/";
@@ -580,14 +600,21 @@ namespace TrakHound.Blazor.Components.ObjectExplorerInternal
             {
                 if (_client != null)
                 {
-                    _uuid = uuid;
-                    _previousUuid = uuid;
+                    _uuid = null;
+                    _previousUuid = null;
                     _query = null;
                     _previousQuery = null;
                     _selectedObject = null;
                     _targetUuids.Clear();
+                    _expandedNamespaces.Clear();
+                    _expandedObjects.Clear();
                     _objects.Clear();
                     _objectParents.Clear();
+                    _objectChildCount.Clear();
+                    _definitions.Clear();
+                    _definitionParents.Clear();
+                    _definitionDescriptions.Clear();
+                    _sourceChains.Clear();
 
                     var objects = await _client.System.Entities.Objects.ReadByUuid(new string[] { _uuid });
                     if (!objects.IsNullOrEmpty())
@@ -1028,7 +1055,7 @@ namespace TrakHound.Blazor.Components.ObjectExplorerInternal
 
         public void UpdateValue(string objectUuid)
         {
-            if (ValueUpdated != null) ValueUpdated.Invoke(this, objectUuid);
+            //if (ValueUpdated != null) ValueUpdated.Invoke(this, objectUuid);
         }
 
 
@@ -1393,70 +1420,98 @@ namespace TrakHound.Blazor.Components.ObjectExplorerInternal
             }
         }
 
-        private void ContentUpdateThrottleElapsed(object sender, EventArgs e)
-        {
-            var updates = new List<ContentUpdate>();
-            for (var i = 0; i < 1000; i++)
-            {
-                _contentUpdated.TryDequeue(out var update);
-                if (update != null) updates.Add(update);
-                else break;
-            }
+        //private void ContentUpdateThrottleElapsed(object sender, EventArgs e)
+        //{
+        //    var updates = new List<ContentUpdate>();
+        //    for (var i = 0; i < 1000; i++)
+        //    {
+        //        _contentUpdated.TryDequeue(out var update);
+        //        if (update != null) updates.Add(update);
+        //        else break;
+        //    }
 
-            if (!updates.IsNullOrEmpty())
-            {
-                var now = UnixDateTime.Now;
+        //    if (!updates.IsNullOrEmpty())
+        //    {
+        //        var now = UnixDateTime.Now;
 
-                var objectUuids = new HashSet<string>();
-                lock (_lock)
-                {
-                    foreach (var update in updates)
-                    {
-                        var ts = update.Timestamp > 0 ? update.Timestamp : now;
+        //        var objectUuids = new HashSet<string>();
+        //        lock (_lock)
+        //        {
+        //            foreach (var update in updates)
+        //            {
+        //                var ts = update.Timestamp > 0 ? update.Timestamp : now;
 
-                        if (update.Entity != null)
-                        {
-                            _content.Remove(update.ObjectUuid);
-                            _content.Add(update.ObjectUuid, update.Entity);
-                        }
+        //                if (update.Entity != null)
+        //                {
+        //                    _content.Remove(update.ObjectUuid);
+        //                    _content.Add(update.ObjectUuid, update.Entity);
+        //                }
 
-                        _values.Remove(update.ObjectUuid);
-                        _values.Add(update.ObjectUuid, update.Value?.Trim());
+        //                _values.Remove(update.ObjectUuid);
+        //                _values.Add(update.ObjectUuid, update.Value?.Trim());
 
-                        if (now - ts < _recentLimit)
-                        {
-                            _recentValues.Remove(update.ObjectUuid);
-                            _recentValues.Add(update.ObjectUuid, ts);
-                        }
+        //                if (now - ts < _recentLimit)
+        //                {
+        //                    _recentValues.Remove(update.ObjectUuid);
+        //                    _recentValues.Add(update.ObjectUuid, ts);
+        //                }
 
-                        objectUuids.Add(update.ObjectUuid);
-                    }
-                }
+        //                objectUuids.Add(update.ObjectUuid);
+        //            }
+        //        }
 
-                if (objectUuids.Count() > 5)
-                {
-                    if (Updated != null) Updated.Invoke(this, EventArgs.Empty);
-                }
-                else
-                {
-                    foreach (var objectUuid in objectUuids)
-                    {
-                        if (ValueUpdated != null) ValueUpdated.Invoke(this, objectUuid);
-                    }
-                }
-            }
-        }
+        //        if (objectUuids.Count() > 5)
+        //        {
+        //            //if (Updated != null) Updated.Invoke(this, EventArgs.Empty);
+        //        }
+        //        else
+        //        {
+        //            foreach (var objectUuid in objectUuids)
+        //            {
+        //                if (ValueUpdated != null) ValueUpdated.Invoke(this, objectUuid);
+        //            }
+        //        }
+        //    }
+        //}
 
         private void ConsumerValueUpdated(string objectUuid, ITrakHoundEntity entity, string value, long timestamp = 0)
         {
-            var update = new ContentUpdate();
-            update.ObjectUuid = objectUuid;
-            update.Entity = entity;
-            update.Value = value;
-            update.Timestamp = timestamp;
+            if (objectUuid != null)
+            {
+                var now = UnixDateTime.Now;
+                var ts = timestamp > 0 ? timestamp : now;
 
-            _contentUpdated.Enqueue(update);
-            _contentUpdateThrottle.Set();
+                lock (_lock)
+                {
+                    if (entity != null)
+                    {
+                        _content.Remove(objectUuid);
+                        _content.Add(objectUuid, entity);
+                    }
+
+                    _values.Remove(objectUuid);
+                    _values.Add(objectUuid, value?.Trim());
+
+                    if (now - ts < _recentLimit)
+                    {
+                        _recentValues.Remove(objectUuid);
+                        _recentValues.Add(objectUuid, ts);
+                    }
+                }
+
+                if (ValueUpdated != null) ValueUpdated.Invoke(this, objectUuid);
+            }
+
+
+           
+            //var update = new ContentUpdate();
+            //update.ObjectUuid = objectUuid;
+            //update.Entity = entity;
+            //update.Value = value;
+            //update.Timestamp = timestamp;
+
+            //_contentUpdated.Enqueue(update);
+            //_contentUpdateThrottle.Set();
         }
 
         private async Task LoadContentWorker(IEnumerable<ITrakHoundObjectEntity> objs)
