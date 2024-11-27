57bb0616dcd2368d79b38afbc15e178eda77843a
Patrick Ritchie <Patrick@TrakHound.com>
2024-11-27 14:03:29 -0500

    Added AssemblyInfo.cs file for specifying version

---
+++ build/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+// Copyright (c) 2024 TrakHound Inc., All Rights Reserved.
+// TrakHound Inc. licenses this file to you under the MIT license.
+
+using System.Reflection;
+
+[assembly: AssemblyVersion("0.1.4.1")]
+[assembly: AssemblyFileVersion("0.1.4.1")]
+[assembly: AssemblyCompany("TrakHound Inc.")]
+[assembly: AssemblyCopyright("Copyright (c) 2024 TrakHound Inc., All Rights Reserved.")]
