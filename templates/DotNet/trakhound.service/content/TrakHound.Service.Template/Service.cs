872b7792d3db69df2b98ae111d186e955b8ea676
Patrick Ritchie <Patrick@TrakHound.com>
2024-12-01 14:13:32 -0500

    Added Service template

---
+++ templates/DotNet/trakhound.service/content/TrakHound.Service.Template/Service.cs
@@ -0,0 +1,20 @@
+﻿using TrakHound;
+using TrakHound.Clients;
+using TrakHound.Services;
+using TrakHound.Volumes;
+
+public class Service : TrakHoundService
+{
+    public Service(ITrakHoundServiceConfiguration configuration, ITrakHoundClient client, ITrakHoundVolume volume) : base(configuration, client, volume) { }
+
+
+    protected override void OnStart()
+    {
+        Log(TrakHoundLogLevel.Information, "UserPackageId Service STARTED.");
+    }
+
+    protected override void OnStop()
+    {
+        Log(TrakHoundLogLevel.Information, "UserPackageId Service STOPPED.");
+    }
+}
