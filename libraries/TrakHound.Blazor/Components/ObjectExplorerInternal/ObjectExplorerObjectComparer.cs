c2d77138a8229c0bc533dd527a5f1ae9ab80378b
Patrick Ritchie <Patrick@TrakHound.com>
2024-12-11 11:53:23 -0500

    Updated Object Explorer Tree View sorting

--- libraries/TrakHound.Blazor/Components/ObjectExplorerInternal/ObjectExplorerObjectComparer.cs
+++ libraries/TrakHound.Blazor/Components/ObjectExplorerInternal/ObjectExplorerObjectComparer.cs
@@ -6,10 +6,70 @@ namespace TrakHound.Blazor.Components.ObjectExplorerInternal
     {
         public int Compare(ITrakHoundObjectEntity x, ITrakHoundObjectEntity y)
         {
-            var xPath = x.GetAbsolutePath();
-            var yPath = y.GetAbsolutePath();
+            if (IsDirectory(x) && !IsDirectory(y))
+            {
+                return 1;
+            }
+            else if (!IsDirectory(x) && IsDirectory(y))
+            {
+                return -1;
+            }
+            else
+            {
+                var xPath = x.GetAbsolutePath();
+                var yPath = y.GetAbsolutePath();
 
-            return string.Compare(xPath, yPath);
+                var xParentPath = TrakHoundPath.GetParentPath(xPath);
+                var yParentPath = TrakHoundPath.GetParentPath(xPath);
+
+                if (xParentPath == yParentPath)
+                {
+                    var xName = TrakHoundPath.GetObject(xPath);
+                    var yName = TrakHoundPath.GetObject(yPath);
+
+                    if (x != null && y != null)
+                    {
+                        var xV = xName.ToVersion();
+                        var yV = yName.ToVersion();
+
+                        if (xV != null && yV != null)
+                        {
+                            if (xV < yV) return -1;
+                            if (xV > yV) return 1;
+                            if (xV == yV) return 0;
+                        }
+                        else if (x.IsNumeric() && y.IsNumeric())
+                        {
+                            var xD = x.ToDouble();
+                            var yD = y.ToDouble();
+
+                            if (xD < yD) return -1;
+                            if (xD > yD) return 1;
+                            if (xD == yD) return 0;
+                        }
+                        else
+                        {
+                            return string.Compare(xName, yName);
+                        }
+                    }
+                    else if (x != null)
+                    {
+                        return 1;
+                    }
+                }
+                else
+                {
+                    return string.Compare(xPath, yPath);
+                }
+            }
+
+            return 0;
+        }
+
+        private static bool IsDirectory(ITrakHoundObjectEntity entity)
+        {
+            if (entity != null) return entity.ContentType == TrakHoundObjectContentTypes.Directory;
+            return false;
         }
     }
 
